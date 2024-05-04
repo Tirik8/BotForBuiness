e46911c119e8930b935cf3dbe75a63c412c5685f
Tirik8 <35536897+Tirik8@users.noreply.github.com>
2024-05-04 19:23:47 +0300

    add README

---
+++ README.md
@@ -0,0 +1,12 @@
+# ---OK---- TODO: env vars for bot config 
+# ---OK---- TODO: rewrite bot to async
+# ---OK---- TODO: include redis
+# ---OK---- TODO: include advanced logging
+# ---30%--- TODO: database with SQLalchemy ORM (async) [subscribes, price list, messages, users, users configs]?
+# TODO: config menu for subscribers with configurate answers (regular expression?)
+# TODO: inline buttons (with another library?)
+# TODO: telegram payments for subscribe
+# TODO: admins for bot? (admins can subscribe, unsubscribe, edit price list, edit config)
+# TODO: handlers for voice, video and picture messages
+# TODO: fields for resend messages and answers
+# TODO: hadnler for rewited messages
--- main.py
+++ main.py
@@ -14,17 +14,3 @@ if __name__ == "__main__":
         asyncio.run(main())
     except KeyboardInterrupt:
         pass
-
-
-# ---OK---- TODO: env vars for bot config 
-# ---OK---- TODO: rewrite bot to async
-# ---30%--- TODO: database with SQLalchemy ORM (async) [subscribes, price list, messages, users, users configs]?
-# ---OK---- TODO: include redis
-# ---OK---- TODO: include advanced logging
-# TODO: config menu for subscribers with configurate answers (regular expression?)
-# TODO: inline buttons (with another library?)
-# TODO: telegram payments for subscribe
-# TODO: admins for bot? (admins can subscribe, unsubscribe, edit price list, edit config)
-# TODO: handlers for voice, video and picture messages
-# TODO: fields for resend messages and answers
-# TODO: hadnler for rewited messages
