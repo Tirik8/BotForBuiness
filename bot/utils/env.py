b9f81b54423e491773ca34ac3860b12510dcc6a7
Tirik8 <35536897+Tirik8@users.noreply.github.com>
2024-05-04 19:10:24 +0300

    can get token in first param(optional)

--- bot/utils/env.py
+++ bot/utils/env.py
@@ -1,9 +1,12 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 
 def load_env():
-    dotenv_path = os.path.join(os.getcwd(), ".env")
-    if os.path.exists(dotenv_path):
-        load_dotenv(dotenv_path)
+    if sys.argv[1]:
+        os.environ['BOT_API_KEY'] = sys.argv[1]
     else:
-        raise FileNotFoundError(dotenv_path)
+        dotenv_path = os.path.join(os.getcwd(), ".env")
+        if os.path.exists(dotenv_path):
+            load_dotenv(dotenv_path)
+        else:
+            raise FileNotFoundError(dotenv_path)
