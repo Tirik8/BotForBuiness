74617ad808b7c4210e92838a9a33bd6ef99877d5
Tirik8 <35536897+Tirik8@users.noreply.github.com>
2024-05-04 18:15:47 +0300

    update todo

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from app.utils.logs import start_logging
 from app.main import main
 
 if __name__ == "__main__":
+
     load_dotenv(".env")
-    
+
     start_logging()
 
     try:
@@ -16,9 +17,14 @@ if __name__ == "__main__":
 
 
 # ---OK---- TODO: env vars for bot config 
-# ---50%--- TODO: rewrite bot to async
-# ---30%---TODO: database with SQLalchemy ORM (async) [subscribes, price list, messages, users, users configs]?
+# ---OK---- TODO: rewrite bot to async
+# ---30%--- TODO: database with SQLalchemy ORM (async) [subscribes, price list, messages, users, users configs]?
+# ---OK---- TODO: include redis
+# ---OK---- TODO: include advanced logging
 # TODO: config menu for subscribers with configurate answers (regular expression?)
 # TODO: inline buttons (with another library?)
 # TODO: telegram payments for subscribe
 # TODO: admins for bot? (admins can subscribe, unsubscribe, edit price list, edit config)
+# TODO: handlers for voice, video and picture messages
+# TODO: fields for resend messages and answers
+# TODO: hadnler for rewited messages
