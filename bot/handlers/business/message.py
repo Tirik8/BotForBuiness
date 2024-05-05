27ef46db47aea88e786a326e2c19a551671596d0
Tirik8 <35536897+Tirik8@users.noreply.github.com>
2024-05-05 19:07:34 +0300

    now adds in db answers

--- bot/handlers/business/message.py
+++ bot/handlers/business/message.py
@@ -1,15 +1,14 @@
 from aiogram import Router, Bot
 from aiogram.types import Message
 from bot.database.requests import add_business_message
-
-import re
+from bot.utils.answer import answer
 
 router = Router()
 
 @router.business_message()
 async def handler_business_message(message: Message, bot: Bot):
     business_connection = await bot.get_business_connection(message.business_connection_id)
-    bm = {
+    business_message = {
         "from_user_id": message.from_user.id,
         "message_id": message.message_id,
         "chat_id": message.chat.id,
@@ -18,12 +17,19 @@ async def handler_business_message(message: Message, bot: Bot):
         "time": message.date,
     }
 
-    await add_business_message(bm)
+    await add_business_message(business_message)
 
     if message.from_user.id != business_connection.user.id:
-        if message.text =='сак' or message.text == 'Сак':
-            await message.answer("Сам сак")
-        elif re.fullmatch("•*", message.text):
-            await message.answer(message.text)
-        elif re.fullmatch("[М, м][Д, д][Э, э, А, а, Е, е]*", message.text):
-            await message.answer(message.text)
+        ans = answer(message.text)
+        if ans:
+            message_answer = await message.answer(ans)
+            
+            business_message_answer = {
+                "from_user_id": message_answer.from_user.id,
+                "message_id": message_answer.message_id,
+                "chat_id": message_answer.chat.id,
+                "business_user_id": business_connection.user.id,
+                "text": message_answer.text,
+                "time": message_answer.date,
+            }
+            await add_business_message(business_message_answer)
---
+++ bot/utils/answer.py
@@ -0,0 +1,8 @@
+import re
+
+def answer(message: str) -> str | None:
+    if message =='сак' or message == 'Сак':
+            return "Сам сак"
+    elif re.fullmatch("•*", message) or re.fullmatch("[М, м][Д, д][Э, э, А, а, Е, е]*", message):
+        return message
+    return None
