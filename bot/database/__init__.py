12a0a17dadfa7461846123c0f4cea26c1c6030e9
Tirik8 <35536897+Tirik8@users.noreply.github.com>
2024-05-06 16:14:44 +0300

    bugfix

--- bot/database/__init__.py
+++ bot/database/__init__.py
@@ -5,7 +5,7 @@ __all__ = (
 
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from config import settings
-from .models.Base import Base
+from .models import Base
 
 engine = create_async_engine(settings.DATABASE_URI)
 async_session = async_sessionmaker(engine)
